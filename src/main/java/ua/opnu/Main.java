fd78e1fdabfa03a83a554e9cea9d592b4432ece3
ai-244-vaulin <9219078@stud.op.edu.ua>
2025-10-29 21:50:39 +0200

    task1 done

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -1,9 +1,23 @@
 package ua.opnu;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
+import java.util.function.Predicate;
 
+public class Main {
+    public static void main(String[] args) {
+
+        // task1
+        Predicate<Integer> isPrime = n -> {
+            if (n < 2) return false;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) return false;
+            }
+            return true;
+        };
+
+        int[] testNumbers = {1, 2, 3, 4, 5, 10, 13, 17, 18};
+
+        for (int n : testNumbers) {
+            System.out.printf("%d -> %b%n", n, isPrime.test(n));
+        }
     }
 }
