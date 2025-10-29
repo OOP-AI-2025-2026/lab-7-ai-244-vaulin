09d211ec1e2bccab348a24e8de4194e371bb8893
ai-244-vaulin <9219078@stud.op.edu.ua>
2025-10-30 00:18:52 +0200

    fix3 pom. Forget to do 4th task

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -4,16 +4,17 @@ import java.util.function.*;
 import java.util.*;
 
 public class Main {
+
     static Student[] filter(Student[] students, Predicate<Student> p) {
-        return Arrays.stream(students)
-                .filter(p)
-                .toArray(Student[]::new);
+        return Arrays.stream(students).filter(p).toArray(Student[]::new);
     }
 
     static int[] filter(int[] arr, Predicate<Integer> p1, Predicate<Integer> p2) {
-        return java.util.Arrays.stream(arr)
-                .filter(i -> p1.test(i) && p2.test(i))
-                .toArray();
+        return Arrays.stream(arr).filter(i -> p1.test(i) && p2.test(i)).toArray();
+    }
+
+    static void forEach(Student[] students, Consumer<Student> action) {
+        for (Student s : students) action.accept(s);
     }
 
     static void doIf(int[] arr, Predicate<Integer> condition, Consumer<Integer> action) {
@@ -37,68 +38,52 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // task1
         Predicate<Integer> isPrime = n -> {
             if (n < 2) return false;
-            for (int i = 2; i <= Math.sqrt(n); i++) {
+            for (int i = 2; i <= Math.sqrt(n); i++)
                 if (n % i == 0) return false;
-            }
             return true;
         };
-
         int[] testNumbers = {1, 2, 3, 4, 5, 10, 13, 17, 18};
-
-        for (int n : testNumbers) {
+        System.out.println("task1:");
+        for (int n : testNumbers)
             System.out.printf("%d -> %b%n", n, isPrime.test(n));
-        }
 
-        // task2
         Student[] students = {
                 new Student("S1", "G1", new int[]{80, 90, 75}),
                 new Student("S2", "G2", new int[]{50, 40, 70}),
-                new Student("3", "G3", new int[]{60, 60, 60})
+                new Student("S3", "G3", new int[]{60, 60, 60})
         };
-
         Predicate<Student> hasDebt = s -> {
             for (int mark : s.getMarks())
                 if (mark < 60) return true;
             return false;
         };
-
-        Student[] filtered = filter(students, hasDebt);
-        for (Student s : filtered)
+        System.out.println("task2:");
+        for (Student s : filter(students, hasDebt))
             System.out.println(s.getName());
 
-
-        // task3
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
         Predicate<Integer> even = n -> n % 2 == 0;
         Predicate<Integer> greaterThan3 = n -> n > 3;
+        System.out.println("task3:");
+        System.out.println(Arrays.toString(filter(nums, even, greaterThan3)));
 
-        int[] result = filter(nums, even, greaterThan3);
-        System.out.println(java.util.Arrays.toString(result));
+        System.out.println("task4:");
+        Consumer<Student> printFullName = s -> System.out.println(s.getName() + " " + s.getGroup());
+        forEach(students, printFullName);
 
-        // task4
-
-        // task5
         int[] nums2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
         Predicate<Integer> isEven = n -> n % 2 == 0;
         Consumer<Integer> printer = n -> System.out.println("Even: " + n);
-
+        System.out.println("task5:");
         doIf(nums2, isEven, printer);
 
-        // task6
         Function<Integer, Integer> powerOfTwo = n -> (int) Math.pow(2, n);
-
         int[] nums3 = {0, 1, 2, 3, 4, 5};
-        int[] result2 = process(nums3, powerOfTwo);
+        System.out.println("task6:");
+        System.out.println(Arrays.toString(process(nums3, powerOfTwo)));
 
-        System.out.println(java.util.Arrays.toString(result2));
-
-
-        // task7
         Function<Integer, String> numToWord = n -> switch (n) {
             case 0 -> "zero";
             case 1 -> "one";
@@ -110,11 +95,10 @@ public class Main {
             case 7 -> "seven";
             case 8 -> "eight";
             case 9 -> "nine";
-            default -> "ten";
+            default -> "unknown";
         };
-
-        int[] nums4 = {0,1,2,3,4,5,6,7,8,9};
-        String[] result3 = stringify(nums4, numToWord);
-        System.out.println(Arrays.toString(result3));
+        int[] nums4 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("task7:");
+        System.out.println(Arrays.toString(stringify(nums4, numToWord)));
     }
 }
